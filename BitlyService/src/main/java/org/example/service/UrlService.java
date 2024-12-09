bfd8d86f196456f5aae6e8a8b82848f8d125ee93
RoyArunim <royarunimb12@gmail.com>
2024-12-09 16:36:16 +0530

    Added the logic for url generation

--- BitlyService/src/main/java/org/example/service/UrlService.java
+++ BitlyService/src/main/java/org/example/service/UrlService.java
@@ -1,13 +1,17 @@
 package org.example.service;
 
+import org.aspectj.bridge.Message;
 import org.example.model.Url;
 import org.example.repositories.UrlRepository;
 import org.example.repositories.UrlUsageStatsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.MessageDigest;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
+import java.util.Optional;
 
 @Service
 public class UrlService {
@@ -31,12 +35,39 @@ public class UrlService {
 
     }
 
-    public String getOriginalUrl(String shortUrl){
+    public String getOriginalUrl(String shortUrl) throws Exception {
+        Url url = urlRepository.findByShortUrl(shortUrl).orElseThrow(() -> new Exception("Short URL not found"));;
+        if(url.getExpiryDate()!=null || url.getExpiryDate().isAfter(LocalDateTime.now())){
+            throw new Exception("Url is expired");
+        }
+
+        return url.getOriginalUrl();
+
 
     }
 
-    public String generateShortUrl(){
+    public String generateShortUrl(String longUrl){
 
+        try{
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.update(longUrl.getBytes());
+            byte[] digest = messageDigest.digest();
+
+
+            //convert byte array to sign representation
+            BigInteger no = new BigInteger(1,digest);
+            //Convert message digest to hexadecimal value
+            String hashText = no.toString(16);
+            while (hashText.length() < 32) {
+                hashText = "0" + hashText;
+            }
+
+            // Return a shorter substring for URL shortening purposes (e.g., first 8 characters)
+            return hashText.substring(0, 8);
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("MD5 algorithm not found");
+        }
     }
 
 }
